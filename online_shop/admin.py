5a4882cfd35ee5e4457384216c45a081648ce8a6
Madi <madi_gaziz@mail.ru>
2023-07-24 10:29:44 +0600

    Создал админ панель для отображение данных

--- online_shop/admin.py
+++ online_shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Category, Product
+from online_shop.models.product import Category, Product
+from online_shop.models.item_in_cart import ItemInCart
+from online_shop.models.order import Order
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -14,7 +16,25 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class ItemCartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'quantity']
+    list_filter = ['product']
+    search_fields = ['product']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'number_phone', 'address', 'date_and_creation', 'get_products']
+    list_filter = ['number_phone', 'address']
+    search_fields = ['number_phone']
+
+    def get_products(self, obj):
+        return ", ".join([str(product) for product in obj.products.all()])
+
+    get_products.short_description = 'Products in Order'
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-
+admin.site.register(ItemInCart, ItemCartAdmin)
+admin.site.register(Order, OrderAdmin)
 # Register your models here.
