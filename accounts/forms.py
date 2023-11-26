f7055ef724cf85679bd90c21818acb874551de4f
Madi <madi_gaziz@mail.ru>
2023-11-27 00:52:16 +0600

    №47 Added the urls.py file to shorten the code, added the ability to login and register a user, added a registration form and created a register.html file

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
+from django import forms 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import password_validation
+from accounts.models import User
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
@@ -19,3 +20,57 @@ class LoginForm(AuthenticationForm):
             }
         )
     )
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-3'
+            }
+        ),
+        label='First Name',
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-3'
+            }
+        ),
+        label='Second Name'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'form-control mb-3'
+            }
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'form-control mb-3'
+            }
+        ),
+        strip=False,
+        help_text="Enter the same password as before, for verification.",
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mb-3'}),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,38 @@
-from django.urls import reverse_lazy
-from django.contrib.auth import views
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+from django.contrib.auth import views, login
+from django.views.generic import CreateView
+from accounts.forms import LoginForm, RegisterForm
+from accounts.models import User
 
-from accounts.forms import LoginForm
 
-
-class LoginView(views.TemplateView):
+class LoginView(views.LoginView):
     template_name = 'registration/login.html'
     form_class = LoginForm
+
+    def get_success_url(self):
+        return reverse('home')
+
 
 
 
 class LogoutView(views.LogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'registration/register.html'
+    form_class = RegisterForm
+
+    def form_valid(self, form):
+
+        user = form.save()
+
+        login(self.request, user)
+
+        return reverse('home')
+    
+    def form_invalid(self, form):
+        print(form.errors)
+        return redirect('home')
